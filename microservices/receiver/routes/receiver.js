3ab621ab5bd9bf58f20e5cef7439d1962489e72a
bilalarifsiddiqui <bilal.arif.siddiqui@gmail.com>
2020-01-03 18:57:04 +0500

    2 microservices

---
+++ microservices/receiver/routes/receiver.js
@@ -0,0 +1,15 @@
+// var express = require('express');
+// var router = express.Router();
+
+// var sendobj = {
+//     firstName: "John",
+//     lastName : "waqar",
+//     id       : 5566
+//   };
+
+// /* GET users listing. */
+// router.post('/sendobj', function(req, res, next) {
+//   req.send(sendobj);
+// });
+
+// module.exports = router;
---
+++ microservices/sender/routes/sender.js
@@ -0,0 +1,27 @@
+// var express = require('express');
+// var router = express.Router();
+
+
+// // /* GET receivers listing. */
+// // router.get('/', function(req, res, next) {
+// //   res.send('respond with a receiver');
+// // });
+
+// var app = express()
+// var request = require('request');
+// var sendobj = {
+//     firstName: "John",
+//     lastName : "waqar",
+//     id       : 5566,
+//   };
+// app.get('/', function (req, res) {
+//   req.post("http://localhost:4000",{getResponse:true,
+//   data:{
+//   sendobj:sendobj
+//   }})
+//   //res.send(sendobj)
+// })
+
+// app.listen(3000, function () {
+//   console.log('Example send listening on port 3000!')
+// })
